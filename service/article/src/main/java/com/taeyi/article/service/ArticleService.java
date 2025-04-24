3cff66ce60b41f68e92f81a69661d72de960207a
belle <ehoi.loveyourself@gmail.com>
2025-04-24 23:26:16 +0900

    feat: article 서비스 Create 작성

---
+++ service/article/src/main/java/com/taeyi/article/service/ArticleService.java
@@ -0,0 +1,27 @@
+package com.taeyi.article.service;
+
+import com.taeyi.article.entity.Article;
+import com.taeyi.article.repository.ArticleRepository;
+import com.taeyi.article.service.request.ArticleCreateRequest;
+import com.taeyi.article.service.response.ArticleResponse;
+import kuke.board.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ArticleService {
+
+    private final Snowflake snowflake = new Snowflake();
+    private final ArticleRepository articleRepository;
+
+    @Transactional
+    public ArticleResponse create(ArticleCreateRequest request) {
+        Article article = articleRepository.save(
+                Article.create(snowflake.nextId(), request.getTitle(), request.getContent(), request.getBoardId(), request.getWriterId())
+        );
+
+        return ArticleResponse.from(article);
+    }
+}
