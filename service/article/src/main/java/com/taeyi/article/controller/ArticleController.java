9dfcfffda95fba21c3556e90ca3b55a521b928fc
belle <ehoi.loveyourself@gmail.com>
2025-04-25 19:35:54 +0900

    feat: article 컨트롤러 작성

---
+++ service/article/src/main/java/com/taeyi/article/controller/ArticleController.java
@@ -0,0 +1,35 @@
+package com.taeyi.article.controller;
+
+import com.taeyi.article.service.ArticleService;
+import com.taeyi.article.service.request.ArticleCreateRequest;
+import com.taeyi.article.service.request.ArticleUpdateRequest;
+import com.taeyi.article.service.response.ArticleResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class ArticleController {
+
+    private final ArticleService articleService;
+
+    @GetMapping("/v1/articles/{articleId}")
+    public ArticleResponse getArticle(@PathVariable Long articleId) {
+        return articleService.read(articleId);
+    }
+
+    @PostMapping("/v1/articles")
+    public ArticleResponse createArticle(@RequestBody ArticleCreateRequest request) {
+        return articleService.create(request);
+    }
+
+    @PatchMapping("/v1/articles/{articleId}")
+    public ArticleResponse updateArticle(@PathVariable Long articleId, @RequestBody ArticleUpdateRequest request) {
+        return articleService.update(articleId, request);
+    }
+
+    @DeleteMapping("/v1/articles/{articleId}")
+    public void deleteArticle(@PathVariable Long articleId) {
+        articleService.delete(articleId);
+    }
+}
